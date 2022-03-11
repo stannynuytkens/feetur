69f8ef55dca2fd4d15207ff4d1939686f85f5571
Stanny Nuytkens <85672679+stannynuytkens@users.noreply.github.com>
2022-03-11 15:18:13 +0100

    we need MORE projects!

---
+++ Feetur.Api.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Feetur.Api.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
